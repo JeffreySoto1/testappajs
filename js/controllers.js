f9e4fa09aa23929e282a5c2e6f11b1e46cf112e3
Jeffrey Soto <Jeff@Jeff-PC>
2014-04-30 16:23:19 -0400

    initial commit

--- js/controllers.js
+++ js/controllers.js
@@ -4,14 +4,14 @@
 
 angular.module('myApp.controllers', [])
         .controller('MyCtrl1', function($scope, $http) {
-
+            
             $scope.allFeeds = [];
 
             $http.get('http://metroclick.us:8080/facebookfeeds/?screenName=TheTonyAwards').success(function(data) {
                 angular.forEach(data, function(value) {
 
                     this.push(value);
-                }, $scope.allFeeds)
+                }, $scope.allFeeds);
                
             });
             $http.get('http://metroclick.us:8080/twitterfeeds/?screenName=TheTonyAwards').success(function(data) {
@@ -19,7 +19,7 @@ angular.module('myApp.controllers', [])
                 angular.forEach(data, function(value) {
 
                     this.push(value);
-                }, $scope.allFeeds)
+                }, $scope.allFeeds);
             });
 
             $http.get('http://metroclick.us:8080/instagramfeeds/?screenName=TheTonyAwards').success(function(data) {
@@ -27,7 +27,7 @@ angular.module('myApp.controllers', [])
                 angular.forEach(data, function(value) {
 
                     this.push(value);
-                }, $scope.allFeeds)
+                }, $scope.allFeeds);
             });
 
 
